eb5c3917e2c039da0bbec45d7cf4536b731f5ecb
LMO GerritBot <gerrit@libremobileos.com>
2025-09-07 13:16:35 +0000

    Adapt to LMODroid by device-add script

--- lmodroid_wade.mk
+++ lmodroid_wade.mk
@@ -11,7 +11,7 @@ PRODUCT_IS_ATV := true
 $(call inherit-product, device/google/atv/products/atv_base.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_tv.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_tv.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -22,7 +22,7 @@ PRODUCT_DEVICE := wade
 PRODUCT_GMS_CLIENTID_BASE := android-askey-tv
 PRODUCT_MANUFACTURER := askey
 PRODUCT_MODEL := Dynalink TV Box
-PRODUCT_NAME := lineage_wade
+PRODUCT_NAME := lmodroid_wade
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="sti6130d350-user 12 SC 20240723 release-keys" \
